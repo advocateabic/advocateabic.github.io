2d6fe822f7a190807b4864895c5721fa34370f74
Michael Gathara <mikegtrm@gmail.com>
2019-11-09 20:31:40 -0600

    Added think page

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,3 @@
-var screenWidth = screen.width;
-if(screen.width >= 414) {
-  document.getElementById("copyrightTop").style.visibility="hidden";
-  document.getElementById("copyrightTop").style.display="none";
-  console.log("Hmmmmmmmmmmmmmmmmm");
-} else {
-  document.getElementById("copyrightBottom").style.visibility="hidden";
-  document.getElementById("copyrightBottom").style.display="none";
-}
-
 function togglediv() {
   var div = document.getElementById("heart");
   if(div.style.display == "none") {
