e188c19f69cba92dbc9ca72383c4baafa9805b2a
Michael Gathara <mikegtrm@gmail.com>
2019-11-19 17:26:59 -0600

    Update

--- assets/index.js
+++ assets/index.js
@@ -10,11 +10,16 @@ function togglediv() {
 }
 document.getElementById("toggle-heart").addEventListener('click', function() {
   var love = document.getElementById("text");
+  var fireWorks = document.getElementById("pyro");
   if(love.style.display == "none") {
     love.style.visibility = "visible";
     love.style.display = "block";
+    fireWorks.style.visibility = "visible";
+    fireWorks.style.display = "block";
   } else {
     love.style.visibility = "hidden";
     love.style.display = "none";
+    fireWorks.style.visibility = "hidden";
+    fireWorks.style.display = "none";
   }
 });
