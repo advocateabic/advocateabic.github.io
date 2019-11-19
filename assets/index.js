e0d0bb47811de9f83521c54b50d5b8e878b94278
Michael Gathara <mikegtrm@gmail.com>
2019-11-18 23:00:23 -0600

    Better

--- assets/index.js
+++ assets/index.js
@@ -8,3 +8,13 @@ function togglediv() {
     div.style.display = "none";
   }
 }
+document.getElementById("toggle-heart").addEventListener('click', function() {
+  var love = document.getElementById("text");
+  if(love.style.display == "none") {
+    love.style.visibility = "visible";
+    love.style.display = "block";
+  } else {
+    love.style.visibility = "hidden";
+    love.style.display = "none";
+  }
+});
