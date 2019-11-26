fe2651db1e6d60340064db877e7278aae61bbad4
Michael Gathara <mikegtrm@gmail.com>
2019-11-26 17:08:01 -0600

    Yeet

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,12 @@
-function togglediv(){var e=document.getElementById("heart");"none"==e.style.display?(e.style.visibility="visible",e.style.display="block"):(e.style.visibility="hidden",e.style.display="none")}document.getElementById("toggle-heart").addEventListener("change",function(){var e=document.getElementById("text"),i=document.getElementById("pyro");"none"==e.style.display?(e.style.visibility="visible",e.style.display="block",i.style.visibility="visible",i.style.display="block"):(e.style.visibility="hidden",e.style.display="none",i.style.visibility="hidden",i.style.display="none")});
+function onLoad() {
+    console.log("Oh hey there. Michael here");
+}
+function togglediv() {
+    var e = document.getElementById("heart");
+    "none" == e.style.display ? (e.style.visibility = "visible", e.style.display = "block") : (e.style.visibility = "hidden", e.style.display = "none")
+}
+document.getElementById("toggle-heart").addEventListener("change", function() {
+    var e = document.getElementById("text"),
+        i = document.getElementById("pyro");
+    "none" == e.style.display ? (e.style.visibility = "visible", e.style.display = "block", i.style.visibility = "visible", i.style.display = "block") : (e.style.visibility = "hidden", e.style.display = "none", i.style.visibility = "hidden", i.style.display = "none")
+});
